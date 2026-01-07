62f400d2af87b306eee69649477dca62a830cbb1
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-01-07 18:57:33 +0530

    Add files via upload

--- EX03/script.js
+++ EX03/script.js
@@ -2,30 +2,54 @@ const xhttp = new XMLHttpRequest();
 
 xhttp.onload = function () {
     const xmlDoc = this.responseXML;
-
-    // ✅ Correct: get all book elements
     const books = xmlDoc.getElementsByTagName("book");
-    let output = "";
+
+    let output = `
+    <table border="1" cellspacing="0" cellpadding="8" class='tbl'>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+    `;
 
     for (let i = 0; i < books.length; i++) {
+        const id = books[i].getElementsByTagName("id")[0].textContent;
         const title = books[i].getElementsByTagName("title")[0].textContent;
         const author = books[i].getElementsByTagName("author")[0].textContent;
+        const category = books[i].getElementsByTagName("category")[0].textContent;
         const price = books[i].getElementsByTagName("price")[0].textContent;
         const status = books[i].getElementsByTagName("status")[0].textContent;
 
         output += `
-            <p>
-                <b>Title:</b> ${title}<br>
-                <b>Author:</b> ${author}<br>
-                <b>Price:</b> ${price}<br>
-                <b>Status:</b> ${status}
-            </p>
-            <hr>
+            <tr>
+                <td>${id}</td>
+                <td>${title}</td>
+                <td>${author}</td>
+                <td>${category}</td>
+                <td>${price}</td>
+                <td>${status}</td>
+            </tr>
         `;
     }
+
+    output += `
+        </tbody>
+    </table>
+    `;
 
     document.getElementById("output").innerHTML = output;
 };
 
 xhttp.open("GET", "library.xml", true);
 xhttp.send();
+
+function add(){
+    alert("Book Added Successfully...");
+}
